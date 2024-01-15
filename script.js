802118cffed7e0a48a78d837a3432020658c0a06
Siddu Ganesh <94279014+sidduganeshsid@users.noreply.github.com>
2024-01-15 15:36:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Replace this with your birth date and time in "YYYY-MM-DDTHH:MM:SS" format
-const birthDate = "2001-01-16T00:00:00";
+const birthDate = "2001-09-09T00:05:05";
 
 function millisecondsToYears(milliseconds) {
     // Define the number of milliseconds in a year
