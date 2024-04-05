c11fa3459e1be866e7a619b2b2a25c465bf4d80c
Siddu Ganesh <94279014+sidduganeshsid@users.noreply.github.com>
2024-04-05 19:34:45 +0530

    Update P script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Replace this with your birth date and time in "YYYY-MM-DDTHH:MM:SS" format
 const birthDate = "2001-09-09T05:05:00";
+const parentsDOM = "2000-04-26T00:00:00";
 
 function millisecondsToYears(milliseconds) {
     // Define the number of milliseconds in a year
@@ -51,11 +52,53 @@ function updateClock(birthDate) {
     document.getElementById('PresentDay').textContent = PresentDayString;
 }
 
+function updateClock(parentsDOM) {
+    const now = new Date();
+    const birthDateTime = (new Date(parentsDOM));//india do = -64803000
+    // const birthDateTime1 = (new Date(birthDate)) - 64803000;
+    const elapsedTime = now - birthDateTime;
+    // const elapsedTime1 = now - birthDateTime1;
+    const milliseconds = elapsedTime1;
+
+    const Pseconds = Math.floor(elapsedTime / 1000) % 60;
+    const Pminutes = Math.floor(elapsedTime / (1000 * 60)) % 60;
+    const Phours = Math.floor(elapsedTime / (1000 * 60 * 60)) % 24;
+    const Pdays = Math.floor(elapsedTime / (1000 * 60 * 60 * 24));
+
+    // const birthYear = birthDateTime.getFullYear();
+    // const currentYear = now.getFullYear();
+    const age = millisecondsToYears(milliseconds);
+
+    const timeString = `${Pdays} days, ${Phours} hours, ${Pminutes} minutes, ${Pseconds} seconds`;
+    // const ageString = "";
+    // if(age==1)
+    // {
+    //     ageString = `Age: ${age} year`;
+    // }
+    // else
+    // {
+    const ageString = `Age: ${age} years`;
+    // }
+    const PresentDayString = `Present Day : ${days+1}`;
+
+    var UserName = "Parents";
+    document.getElementById('Pname').textContent = UserName;
+   // document.getElementById('name').textContent = UserName;
+    document.getElementById('Ptime').textContent = timeString;
+    if(age>=1)
+    document.getElementById('Page').textContent = ageString;
+    else
+    document.getElementById('Page').textContent = "";
+    document.getElementById('PPresentDay').textContent = PresentDayString;
+}
+
 // Update the clock every second
 setInterval(() => {
     updateClock(birthDate);
+    updateClock(parentsDOM);
 }, 1000);
 
 // Initial update
 updateClock(birthDate);
+updateClock(parentsDOM);
 
